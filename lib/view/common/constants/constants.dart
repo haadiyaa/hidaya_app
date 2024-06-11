9bf525a4e6092d7d3f3d2e2b0afb6b87e7c691c4
haadiyaa <haadiyact@gmail.com>
2024-06-11 17:55:34 +0530

    dashboard ui

---
+++ lib/view/common/constants/constants.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class Constants {
+  static const SizedBox height5=SizedBox(height: 5);
+  static const SizedBox height10=SizedBox(height: 10);
+  static const SizedBox height8=SizedBox(height: 8);
+}
---
+++ lib/view/common/styles/textstyle.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+class TextStyles {
+  static const TextStyle dashboardHeadStyle =
+      TextStyle(fontSize: 22, fontWeight: FontWeight.w900, color: Colors.white);
+}
