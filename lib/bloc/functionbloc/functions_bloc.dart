4d6b5346b55e7de26731c22c18633caf7c308d8f
haadiyaa <haadiyact@gmail.com>
2024-07-05 17:01:45 +0530

    check login status

--- lib/bloc/functionbloc/functions_bloc.dart
+++ lib/bloc/functionbloc/functions_bloc.dart
@@ -26,35 +26,30 @@ class FunctionsBloc extends Bloc<FunctionsEvent, FunctionsState> {
       if (isLoggedIn.isNotEmpty) {
         print('sp notempt $isLoggedIn');
         // emit(state.copyWith(status: Status.loggedIn));
-        final Map<String, String>? header = {
-          'Content-Type': 'application/json',
-          'x-auth-token': isLoggedIn,
-        };
-        try {
-          final response = await http.get(
-            Uri.parse('${Secrets.authUrl}${Secrets.getUser}'),
-            headers: header,
-          );
-          print(response.body);
-          final data = jsonDecode(response.body);
-          if (response.statusCode == 200) {
-            user = UserModel.fromMap(data);
-            print('user name: ${user.name}');
-            // emit(GetUserState(user: user));
-            // emit(state.copyWith(user:user));
+        // final Map<String, String>? header = {
+        //   'Content-Type': 'application/json',
+        //   'x-auth-token': isLoggedIn,
+        // };
+        // try {
+          // final response = await http.get(
+          //   Uri.parse('${Secrets.authUrl}${Secrets.getUser}'),
+          //   headers: header,
+          // );
+          // print(response.body);
+          // final data = jsonDecode(response.body);
+          // if (response.statusCode == 200) {
+            // user = UserModel.fromMap(data);
+            // print('user name: ${user.name}');
             print('success');
-            emit(state.copyWith(status: Status.loggedIn,user: user));
-          } else {
-
-            // emit(GetUserErrorState(msg: data['msg']));
-            emit(state.copyWith(status: Status.notLoggedIn));
+            emit(state.copyWith(status: Status.loggedIn));
+          // } else {
+            // emit(state.copyWith(status: Status.notLoggedIn));
             print('not success');
-          }
-        } catch (e) {
-          print('excetion $e');
-          // emit(GetUserErrorState(msg: e.toString()));
-          emit(state.copyWith(status: Status.notLoggedIn));
-        }
+          // }
+        // } catch (e) {
+          // print('excetion $e');
+          // emit(state.copyWith(status: Status.notLoggedIn));
+        // }
       } else {
         print('sp empt $isLoggedIn');
         emit(state.copyWith(status: Status.notLoggedIn));
