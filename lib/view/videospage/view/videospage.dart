eb8b289a2ada644adc55272f711010a77b422303
haadiyaa <haadiyact@gmail.com>
2024-07-16 15:22:50 +0530

    videos page

--- lib/view/videospage/view/videospage.dart
+++ lib/view/videospage/view/videospage.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:hidhayah/utils/constants/constants.dart';
 import 'package:hidhayah/utils/constants/yotubeitems.dart';
 import 'package:hidhayah/view/videospage/view/playerscreen.dart';
+import 'package:hidhayah/view/videospage/widgets/customcurousel.dart';
 import 'package:youtube_player_flutter/youtube_player_flutter.dart';
 
 class VideosPage extends StatelessWidget {
@@ -24,85 +25,10 @@ class VideosPage extends StatelessWidget {
             SizedBox(
               width: size.width,
               height: size.height * 0.3,
-              child: Swiper(
-                autoplay: true,
-                itemCount: videoUrls.length,
-                pagination: const SwiperPagination(),
-                control: const SwiperControl(),
-                itemBuilder: (context, index) {
-                  final videoId =
-                      YoutubePlayer.convertUrlToId(videoUrls[index]);
-                  return GestureDetector(
-                      onTap: () {
-                        Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                                builder: (_) =>
-                                    PlayerScreen(videoId: videoId)));
-                      },
-                      child: Stack(
-                        children: [
-                          Container(
-                            height: size.height * 0.3,
-                            width: size.width,
-                            decoration: BoxDecoration(
-                              image: DecorationImage(
-                                image: NetworkImage(
-                                  YoutubePlayer.getThumbnail(videoId: videoId!),
-                                ),
-                                fit: BoxFit.cover,
-                              ),
-                              color: Constants.greenDark,
-                              borderRadius: BorderRadius.circular(10),
-                            ),
-                          ),
-                          Container(
-                            height: size.height * 0.3,
-                            width: size.width,
-                            decoration: BoxDecoration(
-                              gradient: const LinearGradient(
-                                begin: Alignment.topCenter,
-                                end: Alignment.bottomCenter,
-                                colors: [
-                                  Constants.transparent,
-                                  Constants.transparent,
-                                  Constants.black,
-                                ],
-                              ),
-                              borderRadius: BorderRadius.circular(10),
-                            ),
-                          ),
-                        ],
-                      ));
-                },
-              ),
+              child: CustomCurousel(size: size),
             )
           ],
         ),
-        // child: Padding(
-        //   padding: const EdgeInsets.all(8.0),
-        //   child: ListView.separated(
-        //     separatorBuilder: (context, index) => Constants.height5,
-        //     itemCount: videoUrls.length,
-        //     itemBuilder: (BuildContext context, int index) {
-        //       final videoId=YoutubePlayer.convertUrlToId(videoUrls[index]);
-        //       return GestureDetector(
-        //         onTap: () {
-        //           Navigator.push(context, MaterialPageRoute(builder: (_)=>PlayerScreen(videoId: videoId)));
-        //         },
-        //         child: Container(
-        //           height: size.height * 0.3,
-        //           width: size.width,
-        //           decoration: BoxDecoration(
-        //             image: DecorationImage(image: NetworkImage(YoutubePlayer.getThumbnail(videoId: videoId!),),fit: BoxFit.cover,),
-        //             color: Constants.greenDark,
-        //             borderRadius: BorderRadius.circular(10),
-        //           ),
-        //         ),
-        //       );
-        //     },
-        //   ),
-        // ),
       ),
     );
   }
---
+++ lib/view/videospage/widgets/customcurousel.dart
@@ -0,0 +1,72 @@
+
+import 'package:card_swiper/card_swiper.dart';
+import 'package:flutter/material.dart';
+import 'package:hidhayah/utils/constants/constants.dart';
+import 'package:hidhayah/utils/constants/yotubeitems.dart';
+import 'package:hidhayah/view/videospage/view/playerscreen.dart';
+import 'package:youtube_player_flutter/youtube_player_flutter.dart';
+
+class CustomCurousel extends StatelessWidget {
+  const CustomCurousel({
+    super.key,
+    required this.size,
+  });
+
+  final Size size;
+
+  @override
+  Widget build(BuildContext context) {
+    return Swiper(
+      autoplay: true,
+      itemCount: videoUrls.length,
+      pagination: const SwiperPagination(),
+      control: const SwiperControl(),
+      itemBuilder: (context, index) {
+        final videoId =
+            YoutubePlayer.convertUrlToId(videoUrls[index]);
+        return GestureDetector(
+            onTap: () {
+              Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (_) =>
+                          PlayerScreen(videoId: videoId)));
+            },
+            child: Stack(
+              children: [
+                Container(
+                  height: size.height * 0.3,
+                  width: size.width,
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                      image: NetworkImage(
+                        YoutubePlayer.getThumbnail(videoId: videoId!),
+                      ),
+                      fit: BoxFit.cover,
+                    ),
+                    color: Constants.greenDark,
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                ),
+                Container(
+                  height: size.height * 0.3,
+                  width: size.width,
+                  decoration: BoxDecoration(
+                    gradient: const LinearGradient(
+                      begin: Alignment.topCenter,
+                      end: Alignment.bottomCenter,
+                      colors: [
+                        Constants.transparent,
+                        Constants.transparent,
+                        Constants.black,
+                      ],
+                    ),
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                ),
+              ],
+            ));
+      },
+    );
+  }
+}
