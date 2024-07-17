de40f5c22e01435afed1668defca4ac4857f7480
haadiyaa <haadiyact@gmail.com>
2024-07-17 10:13:21 +0530

    quiz page

---
+++ lib/view/quiz/view/quizpage.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class QuizpageWrapper extends StatelessWidget {
+  const QuizpageWrapper({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const QuizPage();
+  }
+}
+class QuizPage extends StatelessWidget {
+  const QuizPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
